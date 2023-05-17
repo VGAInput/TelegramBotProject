34a62453d467f10ca14869389cfa3a1917cc3d3c
Иван <ivanpaskhin@yandex.ru>
2023-05-17 20:32:12 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotproject/TelegramBotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegrambotproject/TelegramBotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
